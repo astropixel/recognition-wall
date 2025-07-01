5f155ba6d6f9ac94afcac21134dae904868c0ea2
astropixel <paul665@hotmail.com>
2025-07-01 15:36:48 +1000

    Update get-feed.js

--- api/get-feed.js
+++ api/get-feed.js
@@ -1,12 +1,16 @@
 export default async function handler(req, res) {
   const FEED_URL = "https://prf.rewardgateway.com/Feed/Integration/9e8f4544-5edb-43fd-a7dc-5f96a63eb7e5/sndebma0";
-  const r = await fetch(FEED_URL);
-  if (!r.ok) {
-    res.status(500).send("Failed to fetch feed");
-    return;
+  try {
+    const r = await fetch(FEED_URL);
+    if (!r.ok) {
+      res.status(500).send("Failed to fetch feed");
+      return;
+    }
+    const xml = await r.text();
+    res.setHeader("Content-Type", "application/xml");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).send(xml);
+  } catch (err) {
+    res.status(500).send("Server error: " + err.message);
   }
-  const xml = await r.text();
-  res.setHeader("Content-Type", "application/xml");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).send(xml);
 }
